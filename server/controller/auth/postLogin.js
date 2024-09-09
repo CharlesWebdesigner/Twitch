2fef738694f564e7d8145bf18b134756712bbc71
Charles N <vadizcharo@gmail.com>
2024-09-09 19:55:05 +0300

    add security features

--- server/controller/auth/postLogin.js
+++ server/controller/auth/postLogin.js
@@ -1,1 +1,29 @@
-export const postLogin = async (req, res) => {};
+import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const postLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          email,
+        },
+        process.env.Token_key,
+        { expiresIn: "5d" }
+      );
+      return res.status(200).json({
+        userDetails: {
+          email: user.email,
+          username: user.username,
+          token,
+        },
+      });
+    }
+    return res.status(400).send("Invalid credentials please try again");
+  } catch (err) {
+    return res.status(500).send("Something went wrong try again " + err);
+  }
+};
--- server/controller/auth/postRegister.js
+++ server/controller/auth/postRegister.js
@@ -1,10 +1,35 @@
 import User from "../../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const postRegister = async (req, res) => {
-  let body = req.body;
-  const user = await User.create({
-    username: body.username,
-    email: body.email,
-    password: body.password,
-  });
-  return res.status(201).send("user added successfully");
+  try {
+    const { username, email, password } = req.body;
+    const userExists = await User.exists({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "Email already in use" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      username: username,
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email,
+      },
+      process.env.Token_key,
+      { expiresIn: "5d" }
+    );
+    return res.status(201).json({
+      userDetails: {
+        email,
+        username,
+        token,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 };
