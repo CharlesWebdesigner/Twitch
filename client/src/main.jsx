6537bd80b4c86c4f5f87e2a01d6791cab0aef2d1
Charles N <vadizcharo@gmail.com>
2024-09-10 09:39:02 +0300

    initialize client

---
+++ client/src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthPage } from "./AuthPage";
+import { DashboardPage } from "./DashboardPage";
+import { App } from "./App.jsx";
+import "./index.css";
+const router = createBrowserRouter([
+  { path: "/auth", element: <AuthPage /> },
+  { path: "/", element: <DashboardPage /> },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App>
+      <RouterProvider router={router} />
+    </App>
+  </StrictMode>
+);
